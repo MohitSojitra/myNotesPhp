404d43cd3ae09cefdedb0865be1289d6d6939098
Mohit Sojitra <mohitsojitra0@gmail.com>
2019-12-25 19:01:47 +0530

    config added

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+$user = 'root';
+$password = 'root';
+$db = 'myNotes';
+$host = 'localhost';
+$port = 8889;
+
+$link = mysqli_init();
+$success = mysqli_real_connect(
+   $link,
+   $host,
+   $user,
+   $password,
+   $db,
+   $port
+);
+   // if($link)
+   // {
+   //    echo "Connected";
+   // }
+   // else{
+   //    echo "does not connected";
+   // }
+?>
